c37982541aaceea7008d75c72a618d1ed323e73a
Chany <inshakr2@naver.com>
2020-12-20 21:22:21 +0900

    Create Gradle Project

---
+++ tag-catcher/src/main/java/chany/tagcatcher/TagCatcherApplication.java
@@ -0,0 +1,13 @@
+package chany.tagcatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagCatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagCatcherApplication.class, args);
+	}
+
+}
---
+++ tag-catcher/src/test/java/chany/tagcatcher/TagCatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package chany.tagcatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagCatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
