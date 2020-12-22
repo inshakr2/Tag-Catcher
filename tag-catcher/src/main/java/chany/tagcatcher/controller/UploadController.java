5811cc5b14933b072cdbead2d3df573014722178
Chany <inshakr2@naver.com>
2020-12-22 22:57:38 +0900

    Create Upload CSV Service With OpenCSV

--- tag-catcher/src/main/java/chany/tagcatcher/controller/UploadController.java
+++ tag-catcher/src/main/java/chany/tagcatcher/controller/UploadController.java
@@ -1,27 +1,47 @@
 package chany.tagcatcher.controller;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.List;
 
 public class UploadController {
 
-    @PostMapping("/upload-CSV")
+    @PostMapping("/upload-csv")
     public String uploadCSVFile(@RequestParam("file") MultipartFile file, Model model) {
 
         if (file.isEmpty()) {
-            model.addAttribute("err", "Retry Choosing CSV File");
+            model.addAttribute("INFO", "Retry Choosing CSV File");
             model.addAttribute("status", false);
         } else {
 
+            try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
 
+                CsvToBean<String> csvToBean = new CsvToBeanBuilder(reader)
+                        .withIgnoreLeadingWhiteSpace(true)
+                        .build();
 
+                List<String> sentences = csvToBean.parse();
 
+                //TODO -- TO DB
+
+                model.addAttribute("sentences", sentences);
+                model.addAttribute("status", true);
+
+            } catch (IOException e) {
+                model.addAttribute("INFO", "An error occurred while processing the CSV file.");
+                model.addAttribute("status", false);
+            }
         }
 
-        return "";
+        return "upload-csv-status";
     }
 }
