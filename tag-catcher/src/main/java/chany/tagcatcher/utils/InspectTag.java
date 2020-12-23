f299ca36f1e2a64eb06871828e038c15e99b7828
Chany <inshakr2@naver.com>
2020-12-23 21:35:44 +0900

    add getTag Method

--- tag-catcher/src/main/java/chany/tagcatcher/utils/InspectTag.java
+++ tag-catcher/src/main/java/chany/tagcatcher/utils/InspectTag.java
@@ -3,6 +3,10 @@ package chany.tagcatcher.utils;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * inspect
@@ -17,9 +21,31 @@ public class InspectTag {
     // Read Tag.json file
     public static String readTagJson() throws IOException {
         ClassPathResource resource = new ClassPathResource("data/tag.json");
-        String tag = ReadUtils.read(resource);
+        String tagJson = ReadUtils.read(resource);
 
-        return tag;
+        return tagJson;
+    }
+
+    // get Tags to List
+    public static List<String> getTag() throws IOException {
+
+        List<String> tags = new ArrayList<String>();
+        String tagJson = readTagJson();
+
+        String[] tagJsonLines = tagJson.split("\n");
+        Pattern BtagPattern = Pattern.compile("\"B-[a-zA-Z]{3}");
+
+        for(String line : tagJsonLines) {
+            String trimLine = line.trim();
+
+            if(trimLine.startsWith("\"B-")) {
+                Matcher Btag = BtagPattern.matcher(trimLine);
+                Btag.find();
+                tags.add(Btag.group().replace("\"B-", ""));
+            }
+        }
+
+        return tags;
     }
 
 }
