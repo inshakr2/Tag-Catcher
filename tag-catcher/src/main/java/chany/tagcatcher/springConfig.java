1b62ffc9c433486494501ba3ee5b078d6cea9b4e
Chany <inshakr2@naver.com>
2021-01-02 12:59:41 +0900

    configuration Spring Data Config

--- tag-catcher/src/main/java/chany/tagcatcher/springConfig.java
+++ tag-catcher/src/main/java/chany/tagcatcher/springConfig.java
@@ -1,4 +1,21 @@
 package chany.tagcatcher;
 
+import chany.tagcatcher.repository.SentenceRepository;
+import chany.tagcatcher.service.SentenceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class springConfig {
+
+    private final SentenceRepository sentenceRepository;
+
+    @Autowired
+    public springConfig(SentenceRepository sentenceRepository) {
+        this.sentenceRepository = sentenceRepository;
+    }
+
+    public SentenceService sentenceService() {
+        return new SentenceService(sentenceRepository);
+    }
 }
