daa48926b4520a8a0fec0e6440e1e5fbd75d4210
Chany <inshakr2@naver.com>
2021-01-03 23:18:00 +0900

    Service Test - update Sentence, Success

--- tag-catcher/src/test/java/chany/tagcatcher/service/SentenceServiceIntegrationTest.java
+++ tag-catcher/src/test/java/chany/tagcatcher/service/SentenceServiceIntegrationTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @SpringBootTest
 @Transactional
 class SentenceServiceIntegrationTest {
@@ -21,7 +23,7 @@ class SentenceServiceIntegrationTest {
     void 문장등록() {
         Sentence sentence = new Sentence();
         // csvBind 생략
-        sentence.setSentence("안녕하세요. <유창열:PER>입니다. 테스트 문장 등록");
+        sentence.setSentence("안녕하세요. <유창열:PE>입니다. 테스트 문장 등록");
 
         // when
         Long registId = sentenceService.regist(sentence);
@@ -33,7 +35,21 @@ class SentenceServiceIntegrationTest {
         System.out.println("registSentence = " + registSentence.getResult());
         System.out.println("registSentence = " + registSentence.getOrgSentence());
 
+    }
 
+    @Test
+    void 문장수정() {
+        Sentence sentence = new Sentence();
+        sentence.setSentence("안녕하세요, <유창열:P123ER>입니다. Test를 위해 Tag를 이렇게 적었네요.");
+        String updateSentence = "안녕하세요, <유창열:PER>입니다. Test를 위해 문장을 수정해봤어요.";
+
+        //when
+        Long registId = sentenceService.regist(sentence);
+        System.out.println(sentenceService.findOne(registId).get().getSentence());
+        Optional<Sentence> update = sentenceService.updateById(registId, updateSentence);
+
+        //then
+        System.out.println(sentenceService.findOne(registId).get().getSentence());
 
     }
 }
