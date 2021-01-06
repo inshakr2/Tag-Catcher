cc5f4199e99af34232f3c3dd3bf8fe77b3f1b617
Chany <inshakr2@naver.com>
2021-01-06 23:25:30 +0900

    complete delete controller

--- tag-catcher/src/main/java/chany/tagcatcher/controller/UpdateDeleteController.java
+++ tag-catcher/src/main/java/chany/tagcatcher/controller/UpdateDeleteController.java
@@ -1,5 +1,6 @@
 package chany.tagcatcher.controller;
 
+import chany.tagcatcher.domain.Sentence;
 import chany.tagcatcher.service.SentenceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class UpdateDeleteController {
@@ -29,12 +32,19 @@ public class UpdateDeleteController {
     @GetMapping("/delete")
     public String deleteSentence(@RequestParam("id") Long id, Model model) {
 
-        return "redirect:/refresh";
-    }
+        try {
+            sentenceService.deleteOne(id);
+            List<Sentence> sentences = sentenceService.findAll();
 
-    @RequestMapping("/refresh")
-    public String refreshTable(Model model) {
+            model.addAttribute("status", true);
+            model.addAttribute("sentences", sentences);
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("INFO", "an Error occur in delete sentence");
+            model.addAttribute("status", false);
+        }
 
         return "upload-result";
     }
+
 }
