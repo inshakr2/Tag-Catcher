ddeb781e1d5907a4139f18ac4e546249cab25e68
Chany <inshakr2@naver.com>
2021-01-08 00:52:17 +0900

    delete button in index.html

--- tag-catcher/src/main/java/chany/tagcatcher/controller/HomeController.java
+++ tag-catcher/src/main/java/chany/tagcatcher/controller/HomeController.java
@@ -7,11 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("findDB")
-    public String toFindDB(Model model) {
-        return "findDB";
-    }
-
     @GetMapping("upload")
     public String toUpload(Model model) {
         return "upload";
